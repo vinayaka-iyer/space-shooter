d5e0ce7d4f8afd4edc1b9c9b8aca9149b7f5741b
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2024-12-28 17:38:08 +0530

    drawing surfaces and importing images

--- code/main.py
+++ code/main.py
@@ -1,11 +1,29 @@
 import pygame
+from os.path import join
+from random import randint
 
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('Space Shooters')
+pygame.display.set_caption('Space Shooter')
 running = True
+
+# surface
+surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100
+
+# importing an image
+player_surface = pygame.image.load(
+    join('images', 'player.png')).convert_alpha()
+
+# import stars
+star_surface = pygame.image.load(
+    join('images', 'star.png')).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT))
+                  for i in range(20)]
+
 
 while running:
     # event loop
@@ -14,7 +32,12 @@ while running:
             running = False
 
     # draw the game
-    # fill the window with a red color
-    display_surface.fill('red')
+    display_surface.fill('darkgray')
+    for pos in star_positions:
+        display_surface.blit(star_surface, pos)
+
+    x += 0.1
+    display_surface.blit(player_surface, (x, 150))
+
     pygame.display.update()
 pygame.quit()
